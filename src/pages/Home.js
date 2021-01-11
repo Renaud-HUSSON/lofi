ad2afefae281d415c62daa7085183755e3c9d1ad
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-01-11 16:44:28 +0100

    fix(Homepage): weird looking on few mobile devices

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import HomeImages from "../components/Home/HomeImages"
 import HomeText from "../components/Home/HomeText"
 
 const Home = () => {
-  return <div className="home flex h-full items-center justify-around flex-col lg:flex-row">
+  return <div className="home flex h-full items-center justify-center md:justify-around flex-col lg:flex-row">
     <HomeText />
     <HomeImages />
   </div>
