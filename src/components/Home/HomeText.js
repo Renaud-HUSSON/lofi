54dd50ddff551bc3f17b56e6423f6f55adea7a1e
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-01-13 17:40:54 +0100

    fix(player details):  image width is full available width

--- src/components/Home/HomeText.js
+++ src/components/Home/HomeText.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const HomeText = () => {
   return <section className="lg:w-1/2 self-start lg:self-center mb-5">
-    <p className="2xl:text-7xl xl:text-6xl md:text-5xl text-4xl">Do you like <strong>lofi</strong>?</p>
+    <p className="2xl:text-7xl xl:text-6xl md:text-5xl text-4xl">Do you like <strong>lofi</strong> ?</p>
     <p className="2xl:text-7xl xl:text-6xl md:text-5xl text-4xl mb-5">If you do,</p>
     <Link className="transition-colors font-semibold duration-500 inline-block xl:text-3xl lg:text-2xl text-base dark:bg-white bg-black p-4 sm:px-6 sm:py-4 dark:text-black text-white" to="/collection">SEE THE COLLECTION</Link>
   </section>
--- src/components/Player/PlayerDetails.js
+++ src/components/Player/PlayerDetails.js
@@ -4,12 +4,12 @@ const PlayerDetails = ({currentSong, details}) => {
   return currentSong
   ?<div className={`z-10 transition-all overflow-hidden duration-500 player__details fixed right-0 left-0 bg-white dark:bg-black transform ${details ? 'player__details--opened' : ''}`}>
     <div className="flex flex-col md:flex-row justify-center items-center h-full">
-      <div className="md:flex-1">
-        <img className="w-full md:w-3/4 mx-auto" src={currentSong.snippet.thumbnails.medium.url} alt=""/>
+      <div className=" w-full mdflex-1">
+        <img className="w-11/12 md:w-3/4 mx-auto" src={currentSong.snippet.thumbnails.medium.url} alt=""/>
       </div>
-      <div className="md:flex-1">
-        <p className="text-center font-bold md:text-xl xl:text-2xl my-2"><a className="p-2" target='_blank' rel='noreferrer' href={`https://youtube.com/channel/${currentSong.snippet.channelId}`}>{currentSong.snippet.channelTitle}</a></p>
-        <p className="text-center md:text-xl xl:text-2xl my-2"><a className="p-2" target='_blank' rel='noreferrer' href={`https://youtube.com/watch?v=${currentSong.id.videoId}`}>{currentSong.snippet.title}</a></p>
+      <div className=" w-11/12 mdflex-1">
+        <p className="text-center font-bold text-xl xl:text-2xl my-2"><a className="p-2" target='_blank' rel='noreferrer' href={`https://youtube.com/channel/${currentSong.snippet.channelId}`}>{currentSong.snippet.channelTitle}</a></p>
+        <p className="text-center text-xl xl:text-2xl my-2"><a className="p-2" target='_blank' rel='noreferrer' href={`https://youtube.com/watch?v=${currentSong.id.videoId}`}>{currentSong.snippet.title}</a></p>
       </div>
     </div>
   </div>
